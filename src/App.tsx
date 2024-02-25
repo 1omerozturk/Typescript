8b866ab0c2fe89edf79dac33a78386a68b76ec39
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2024-02-25 18:54:08 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Input from './component/input';
+import { Todos } from './Types/types';
+import Message from './component/Message';
 
-function App() {
+const App:React.FC=()=> {
+
+const [todo,setTodo]=useState<string>('')
+const [todos,setTodos]=useState<Todos[]>([])
+
+const addMessage=()=>{
+  if(todo) setTodos([...todos,{message:todo,id:todos.length+1}])
+  setTodo("")
+}
+
+const deleteMessage=(id:number)=>{
+  setTodos(todos.filter(todo=>todo.id!=id))
+}
+
+console.log("Todos: ",todos)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className='text-bold'>
+      To - Do Ap      
+      </h1>
+      <div>
+       <Input addMessage={addMessage} todo={todo} setTodo={setTodo}/>
+       <Message deleteMessage={deleteMessage} todos={todos}/>
+        </div>
     </div>
   );
 }
